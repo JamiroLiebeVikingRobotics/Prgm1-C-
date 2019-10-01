dae4522d80d356f87bff54ede2605f425c89cec7
JamiroLiebeVikingRobotics <54374773+JamiroLiebeVikingRobotics@users.noreply.github.com>
2019-10-01 13:45:33 +0200

    Array Kod

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace arräer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] siffror = new int [5];
+
+            for (int i = 0; i < siffror.GetLength(0); i++)
+            {
+                Console.WriteLine("Enter a number");
+                string p = Console.ReadLine();
+                int.TryParse(p, out int a);
+                siffror[i] = a;
+
+            }
+            Console.WriteLine("Your array in reverse");
+            for (int i = 4; i > -1; i--)
+            {
+                Console.WriteLine(siffror[i]);
+            }
+        }
+    }
+}
